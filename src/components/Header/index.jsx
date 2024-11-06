3c48037205df262f833c26ca48cbb8488d009679
gununes031 <gununes31@yahoo.com.br>
2024-11-05 23:36:53 -0300

    entrega desafio

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import logo from '../../assets/logo-dio.png';
+import { useNavigate  } from "react-router-dom";
 
 import { Button } from '../Button';
 
 import { Container, Wrapper, BuscarInputContainer, Input, Row, Menu, MenuRight, UserPicture} from './styles';
 
 const Header = ({autenticado}) => {
+  const navigate = useNavigate();
+
+  const handleClickSignIn = () => {
+    navigate('/login')
+  }
+  const handleClickSignUp = () => {
+    navigate('/cadastro')
+  }
+
   return (
     <Wrapper>
       <Container>
@@ -23,12 +33,12 @@ const Header = ({autenticado}) => {
           </Row>
           <Row>
               {autenticado ? (
-                <UserPicture src="https://avatars.githubusercontent.com/u/45184516?v=4"/>
+                <UserPicture src="https://avatars.githubusercontent.com/u/62776616?v="/>
               ) : (
               <>
                 <MenuRight href="/">Home</MenuRight>
-                <Button title="Entrar" />
-                <Button title="Cadastrar" />
+                <Button title="Entrar" onClick={handleClickSignIn}/>
+                <Button title="Cadastrar" onClick={handleClickSignUp}/>
               </>)}
           </Row>
       </Container>
---
+++ src/pages/cadastro/index.jsx
@@ -0,0 +1,71 @@
+import { useNavigate  } from "react-router-dom";
+import { MdPerson, MdEmail, MdLock } from 'react-icons/md'
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { api } from '../../services/api';
+
+import { useForm } from "react-hook-form";
+
+
+import { Container, Title, Column, TitleCadastro, SubtitleCadastro, EsqueciText, LoginText, Row, Wrapper, BottontitleCadastro } from './styles';
+
+const Cadastro = () => {
+
+    const navigate = useNavigate()
+
+    const { control, handleSubmit, formState: { errors  } } = useForm({
+        reValidateMode: 'onChange',
+        mode: 'onChange',
+    });
+
+    const onSubmit = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}&nome=${formData.name}`);
+            
+            if(data.length && data[0].id){
+                navigate('/feed') 
+                return
+            }
+
+            alert('Usuário ou senha inválido')
+        }catch(e){
+            //TODO: HOUVE UM ERRO
+            navigate('/feed')
+        }
+    };
+
+    console.log('errors', errors);
+
+    return (<>
+        <Header />
+        <Container>
+            <Column>
+                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                 e entrar mais rápido nas empresas mais desejadas.</Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                <TitleCadastro>Comece agora grátis</TitleCadastro>
+                <SubtitleCadastro>Crie sua conta e make the change._</SubtitleCadastro>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input placeholder="Nome Completo" leftIcon={<MdPerson />} name="name"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    {errors.email && <span>E-mail é obrigatório</span>}
+                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    {errors.senha && <span>Senha é obrigatório</span>}
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
+                </form>
+                <BottontitleCadastro>Ao clicar em "criar minha conta grátis", declaro que aceito as politicas de privacidade e os Termos de Uso da DIO.</BottontitleCadastro>
+                <Row>
+                    <EsqueciText>Já tenho conta.  </EsqueciText>
+                    <LoginText>Fazer login</LoginText>
+                </Row>
+                </Wrapper>
+            </Column>
+        </Container>
+    </>)
+}
+
+export { Cadastro }
